5c7e1cb5583d185ec122f2b5207230e1b658e8b8
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-26 12:15:15 +0500

    Update UserInfo.jsx

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -5,10 +5,10 @@ const { Text } = Typography;
 export const UserInfo = observer(() => {
     return (
         <Flex vertical>
-            //<Text strong>Информация о пользователе</Text>
-            //<Text>Абдулов Даниил Билалович</Text>
-            //<Text>Рост: 176 см</Text>
-            //<Text>Вес: 55 кг</Text>
+            <Text strong>Информация о пользователе</Text>
+            <Text>Абдулов Даниил Билалович</Text>
+            <Text>Рост: 176 см</Text>
+            <Text>Вес: 55 кг</Text>
         </Flex>
     );
 });
