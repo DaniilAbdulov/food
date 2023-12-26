51141e311193dbf8580afe1ab043e0cad4daa625
Daniil Abdulov <abdulov.dany@yandex.ru>
2023-12-26 09:23:49 +0500

    Update productsCategories.js

--- src/data/productsCategories.js
+++ src/data/productsCategories.js
@@ -1,1 +1,1 @@
-export const productsCategories = [{id:1,name:"Мясо"},{id:2,name:"Яйца"},{id:3,name:"Крупы"},]
+export const productsCategories = [{id:1,name:"Мясо"},{id:2,name:"Яйца"},{id:3,name:"Крупы"},{id:4,name:"Молочная продукция"},{id:5,name:"Напитки"},{id:6,name:"Фрукты"},{id:7,name:"Овощи"}]
