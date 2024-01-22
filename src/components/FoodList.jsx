56330dc67cbe3d8e2b74af86d3c840fd54216e80
Daniil Abdulov <abdulov.dany@yandex.ru>
2024-01-22 08:56:03 +0500

    Update FoodList.jsx

--- src/components/FoodList.jsx
+++ src/components/FoodList.jsx
@@ -43,6 +43,7 @@ export const FoodList = observer(() => {
               );
             })}
       </Row>
+      <div>
       <Form.Item>
         <Button type="primary" htmlType="submit">
           Съедено
@@ -53,6 +54,7 @@ export const FoodList = observer(() => {
           Сбросить
         </Button>
       </Form.Item>
+      </div>
     </Form>
   );
 });
